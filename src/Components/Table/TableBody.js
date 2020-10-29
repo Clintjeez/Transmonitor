cf6d882d53ef9c0c549eac3da75c9e11148198fc
clintjeez <clintjeez@gmail.com>
2020-10-29 09:50:43 +0100

    table footer style fix

--- src/Components/Table/TableBody.js
+++ src/Components/Table/TableBody.js
@@ -30,19 +30,23 @@ function TableBody({ payment_data, handlePageClick, pageCount }) {
           </tbody>
         )
       )}
-      <ReactPaginate
-        previousLabel={"prev"}
-        nextLabel={"next"}
-        breakLabel={"..."}
-        breakClassName={"break-me"}
-        pageCount={pageCount}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={5}
-        onPageChange={handlePageClick}
-        containerClassName={"pagination"}
-        subContainerClassName={"pages pagination"}
-        activeClassName={"active"}
-      />
+      <div className="table__footer">
+        <p>Showing 1 to 10 of 500 entries</p>
+        <ReactPaginate
+          previousLabel={"prev"}
+          nextLabel={"next"}
+          breakLabel={"..."}
+          breakClassName={"break-me"}
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageClick}
+          containerClassName={"pagination"}
+          subContainerClassName={"pages pagination"}
+          activeClassName={"active"}
+          className="table__paginate"
+        />
+      </div>
     </>
   );
 }
